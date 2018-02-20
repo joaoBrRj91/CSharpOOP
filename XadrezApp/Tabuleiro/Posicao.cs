48c8a766f57c23bb195545053cb2a6e43e19a669
João Nascimento <joao1991algoritmo@gmail.com>
2018-02-20 20:14:16 -0300

    Criação da classe "Posicao"; pertencente ao tabuleiro de xadrez, na camada de application

---
+++ XadrezApp/Tabuleiro/Posicao.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace XadrezApp.Tabuleiro
+{
+    public class Posicao: IDisposable
+    {
+
+        #region Propriedades
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+        #endregion
+
+        #region Campos Privados
+        private StringBuilder mensagem;
+        #endregion
+
+        #region Construtores
+        public Posicao(int linha, int coluna)
+        {
+            this.Linha = linha;
+            this.Coluna = coluna;
+        }
+        #endregion
+
+        #region Metodos Publicos
+        public override string ToString()
+        {
+            Dispose();
+            mensagem = new StringBuilder();
+            mensagem.AppendFormat("Linha: {0}, Coluna: {1}", Linha,Coluna);
+            return mensagem.ToString();
+        }
+
+        public void Dispose()
+        {
+            mensagem = null;
+        }
+        #endregion
+
+
+    }
+}
--- XadrezGameGUI/Program.cs
+++ XadrezGameGUI/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using XadrezApp.Tabuleiro;
 
 namespace XadrezGameGUI
 {
@@ -10,6 +7,10 @@ namespace XadrezGameGUI
     {
         static void Main(string[] args)
         {
+            Posicao p = new Posicao(linha: 3, coluna: 4);
+
+            Console.WriteLine(p);
+            Console.ReadKey();
         }
     }
 }
